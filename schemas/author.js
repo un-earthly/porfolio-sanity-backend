4f6dea1e2afbb37266ade7658e43fbb3bb9a6008
un-earthly <vijayalamin@gmail.com>
2022-06-23 22:21:42 -0700

    first commit

---
+++ schemas/author.js
@@ -0,0 +1,31 @@
+export default {
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'bio',
+      title: 'Bio',
+      type: 'string'
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+}
---
+++ schemas/blog.js
@@ -0,0 +1,75 @@
+export default {
+    name: 'blog',
+    title: 'Blog',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'content',
+            title: 'Content',
+            type: 'array',
+            of: [
+                {
+                    type: 'block'
+                },
+                {
+                    type: 'image',
+                    fields: [
+                        {
+                            type: 'text',
+                            name: 'alt',
+                            title: 'Alternative text',
+                            description: `Some of your visitors cannot see images, 
+            be they blind, color-blind, low-sighted; 
+            alternative text is of great help for those 
+            people that can rely on it to have a good idea of 
+            what\'s on your page.`,
+                            options: {
+                                isHighlighted: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            title: 'Image',
+            name: 'img',
+            type: 'image',
+            options: {
+                hotspot: true // <-- Defaults to false
+            },
+            fields: [
+                {
+                    name: 'caption',
+                    type: 'string',
+                    title: 'Caption',
+                    options: {
+                        isHighlighted: true // <-- make this field easily accessible
+                    }
+                },
+                {
+                    // Editing this field will be hidden behind an "Edit"-button
+                    name: 'attribution',
+                    type: 'string',
+                    title: 'Attribution',
+                }
+            ]
+        },
+        {
+            name: 'author',
+            title: 'Author',
+            type: 'reference',
+            to: { type: 'author' },
+        },
+        {
+            name: 'timestamp',
+            title: 'Create At',
+            type: 'datetime',
+        },
+    ],
+}
